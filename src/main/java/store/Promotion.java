6360fcd2d6aded3798288aff6934d48ff884507b
SeongbinPark <hyucse22@gmail.com>
2024-11-09 19:50:49 +0900

    feat(Promotion): 프로모션 객체에 필요한 기능 정의

--- src/main/java/store/Promotion.java
+++ src/main/java/store/Promotion.java
@@ -1,4 +1,38 @@
 package store;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDate;
+
 public class Promotion {
+    private final String name;
+    private final int buyQuantity;
+    private final int getQuantity;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public Promotion(String name, int buyQuantity, int getQuantity, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buyQuantity = buyQuantity;
+        this.getQuantity = getQuantity;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBuyQuantity() {
+        return buyQuantity;
+    }
+
+    public int getGetQuantity() {
+        return getQuantity;
+    }
+
+    public boolean isActive() {
+        LocalDate today = DateTimes.now().toLocalDate();
+        return (today.isEqual(startDate) || today.isAfter(startDate)) &&
+                (today.isEqual(endDate) || today.isBefore(endDate));
+    }
 }
