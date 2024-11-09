893c2ef29f5dea7787b7a61af95c3406c92b65e5
SeongbinPark <hyucse22@gmail.com>
2024-11-09 22:16:18 +0900

    feat(InputView): 입력받은 구매물품, 수량을 파싱

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -4,6 +4,7 @@ import static store.util.DataLoader.loadProducts;
 import static store.util.DataLoader.loadPromotions;
 
 import java.util.List;
+import store.model.OrderItem;
 import store.model.Product;
 import store.model.Promotion;
 import store.service.Inventory;
@@ -32,6 +33,8 @@ public class Application {
 
             // 구매할 품목 및 개수를 입력
             String input = inputView.readItemInput();
+
+            List<OrderItem> orderItems = inputView.parseOrderItems(input, inventory);
 
             // 마지막 부분
             outputView.printThankYouMessage();
--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -2,11 +2,16 @@ package store.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.ArrayList;
+import java.util.List;
+import store.model.OrderItem;
+import store.service.Inventory;
+
 public class InputView {
     public String readItemInput() {
         return readLine();
     }
-    
+
     public boolean readYesOrNo() {
         final String input = readLine().trim().toUpperCase();
         while (!input.equals("Y") && !input.equals("N")) {
@@ -18,5 +23,36 @@ public class InputView {
         }
         return "Y".equals(input);
     }
+
+    public List<OrderItem> parseOrderItems(final String input, final Inventory inventory) {
+        final List<OrderItem> items = new ArrayList<>();
+        // 입력에서 중간의 ],[ 만 제거
+        final String[] tokens = input.split("],\\[");
+
+        for (final String token : tokens) {
+            // [ ] 모두 제거 및 -를 기준으로 물품명과 수량을 구분
+            final String cleanedToken = token.replaceAll("[\\[\\]]", "");
+            final String[] parts = cleanedToken.split("-");
+
+            // 물품명-수량 두 부분이 아니면 에러
+            if (parts.length != 2) {
+                throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+            }
+            final String productName = parts[0].trim(); // 물품
+            final int quantity; // 그 물품의 수량
+            try {
+                quantity = Integer.parseInt(parts[1].trim());
+                if (quantity <= 0) {
+                    throw new NumberFormatException();
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 수량은 양의 정수여야 합니다. 다시 입력해 주세요.");
+            }
+
+
+        }
+
+    }
+
 
 }
