02b48670f82426e66d13d5f5385fe6551767f3f9
SeongbinPark <hyucse22@gmail.com>
2024-11-11 06:50:13 +0900

    feat(PromotionService): 프로모션관련 처리 로직 추가

--- src/main/java/store/service/PromotionService.java
+++ src/main/java/store/service/PromotionService.java
@@ -1,4 +1,20 @@
 package store.service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import store.domain.Promotion;
+
 public class PromotionService {
+
+    private final Map<String, Promotion> promotions;
+
+    public PromotionService(List<Promotion> promotions) {
+        this.promotions = promotions.stream()
+                .collect(Collectors.toMap(Promotion::getName, p -> p));
+    }
+
+    public Promotion getPromotion(String promotionType) {
+        return promotions.get(promotionType);
+    }
 }
