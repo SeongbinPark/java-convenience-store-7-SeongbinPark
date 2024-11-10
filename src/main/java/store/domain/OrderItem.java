fa5752b104b5365819f79a9635bd466fa6b13ae3
SeongbinPark <hyucse22@gmail.com>
2024-11-11 00:03:52 +0900

    feat(OrderItem): record 전환
    보일러코드 삭제

--- src/main/java/store/domain/OrderItem.java
+++ src/main/java/store/domain/OrderItem.java
@@ -1,4 +1,8 @@
+// src/main/java/store/domain/OrderItem.java
 package store.domain;
 
-public class OrderItem {
+public record OrderItem(Product product, int quantity) {
+    public int calculateTotalPrice() {
+        return product.getPrice() * quantity;
+    }
 }
