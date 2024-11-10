a103685e911e9754a30eecc80527959f69c8e4b7
SeongbinPark <hyucse22@gmail.com>
2024-11-10 22:15:15 +0900

    feat(OutputView): 프로그램 실행 메세지 출력

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -2,15 +2,8 @@ package store.view;
 
 public class OutputView {
 
-    public void printWelcomeMessage() {
-        System.out.println("안녕하세요. W편의점입니다.");
-    }
-
-    public void printInventoryListMessage() {
+    public void printWelcome() {
+        System.out.println("\n안녕하세요. W편의점입니다.");
         System.out.println("현재 보유하고 있는 상품입니다.\n");
-    }
-
-    public void printThankYouMessage() {
-        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
     }
 }
