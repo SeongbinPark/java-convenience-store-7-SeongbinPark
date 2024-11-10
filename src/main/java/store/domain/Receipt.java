37b4360314e87347cfcb614f56ec2fc30bef7d7c
SeongbinPark <hyucse22@gmail.com>
2024-11-10 23:41:59 +0900

    feat(Receipt): 영수증 클래스 기본 설정

--- src/main/java/store/domain/Receipt.java
+++ src/main/java/store/domain/Receipt.java
@@ -1,4 +1,21 @@
 package store.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Receipt {
+    private final List<OrderItem> orderItems;
+    private final List<FreeItem> freeItems;
+    private final int totalAmount;
+    private final int promotionDiscount;
+    private final int membershipDiscount;
+
+    public Receipt(List<OrderItem> orderItems, List<FreeItem> freeItems,
+                   int totalAmount, int promotionDiscount, int membershipDiscount) {
+        this.orderItems = new ArrayList<>(orderItems);
+        this.freeItems = new ArrayList<>(freeItems);
+        this.totalAmount = totalAmount;
+        this.promotionDiscount = promotionDiscount;
+        this.membershipDiscount = membershipDiscount;
+    }
 }
