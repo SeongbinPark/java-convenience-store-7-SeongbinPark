a7045f95cbef7708e534285d807059a19d94d894
SeongbinPark <hyucse22@gmail.com>
2024-11-10 23:40:00 +0900

    feat(OutputView): printProducts 메서드 추가
    모든 상품 출력

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,12 +1,37 @@
 // src/main/java/store/view/OutputView.java
 package store.view;
 
+import java.util.List;
+import store.domain.Product;
 import store.domain.Receipt;
 
 public class OutputView {
 
     private static final String LINE_SEPARATOR = System.lineSeparator();
 
+    public void printWelcome() {
+        System.out.println("\n안녕하세요. W편의점입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.\n");
+    }
+
+    public void printProducts(List<Product> products) {
+        for (Product product : products) {
+            if (product.getTotalStock() > 0) {
+                System.out.printf("- %s %,d원 %d개%s%n",
+                        product.getName(),
+                        product.getPrice(),
+                        product.getTotalStock(),
+                        product.hasPromotion() ? " " + product.getPromotionType() : "");
+            } else {
+                System.out.printf("- %s %,d원 재고 없음%s%n",
+                        product.getName(),
+                        product.getPrice(),
+                        product.hasPromotion() ? " " + product.getPromotionType() : "");
+            }
+        }
+        System.out.println();
+    }
+    
     public void printReceipt(Receipt receipt) {
         StringBuilder sb = new StringBuilder();
         sb.append("\n===========W 편의점=============").append(LINE_SEPARATOR);
