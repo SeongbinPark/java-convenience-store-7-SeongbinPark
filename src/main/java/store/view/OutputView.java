f50999280fe1bc3d21de006d2f727c777392e394
SeongbinPark <hyucse22@gmail.com>
2024-11-10 22:37:36 +0900

    feat(OutputView): 영수증 출력 문구 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,9 +1,18 @@
+// src/main/java/store/view/OutputView.java
 package store.view;
+
+import store.domain.Receipt;
 
 public class OutputView {
 
-    public void printWelcome() {
-        System.out.println("\n안녕하세요. W편의점입니다.");
-        System.out.println("현재 보유하고 있는 상품입니다.\n");
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
+    public void printReceipt(Receipt receipt) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n===========W 편의점=============").append(LINE_SEPARATOR);
+        sb.append(String.format("%-12s %4s %10s%n", "상품명", "수량", "금액")); // 제목의 너비 조정
+
+        System.out.print(sb);
     }
+
 }
