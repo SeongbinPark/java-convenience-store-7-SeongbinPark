96f6a82c1f57b252e9b0aa6d15efb86cc0ca53e1
SeongbinPark <hyucse22@gmail.com>
2024-11-10 22:16:20 +0900

    feat(Product): Product 클래스 정의 및 getter 추가
    추후 필요없는 getter는 삭제예정. 최대한 적게 써보자.

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,4 +1,43 @@
+// src/main/java/store/domain/Product.java
 package store.domain;
 
-public record Product() {
+public class Product {
+    private final String name;
+    private final int price;
+    private int promotionStock;
+    private int normalStock;
+    private final String promotionType;
+
+    public Product(String name, int price, int promotionStock, int normalStock, String promotionType) {
+        this.name = name;
+        this.price = price;
+        this.promotionStock = promotionStock;
+        this.normalStock = normalStock;
+        this.promotionType = promotionType != null && !"null".equals(promotionType) ? promotionType : "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getPromotionStock() {
+        return promotionStock;
+    }
+
+    public int getNormalStock() {
+        return normalStock;
+    }
+
+    public int getTotalStock() {
+        return promotionStock + normalStock;
+    }
+
+    public String getPromotionType() {
+        return promotionType;
+    }
+
 }
