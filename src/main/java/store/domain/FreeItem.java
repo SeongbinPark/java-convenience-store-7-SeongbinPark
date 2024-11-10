817cdadcfc128d7b420b21c77e8f722dfcd62773
SeongbinPark <hyucse22@gmail.com>
2024-11-10 23:53:00 +0900

    feat(FreeItem): calculateDiscountAmount 메서드 추가
    증정품을 통해 할인액 계산

--- src/main/java/store/domain/FreeItem.java
+++ src/main/java/store/domain/FreeItem.java
@@ -1,4 +1,8 @@
+// src/main/java/store/domain/FreeItem.java
 package store.domain;
 
-public record FreeItem() {
+public record FreeItem(Product product, int quantity) {
+    public int calculateDiscountAmount() {
+        return product.getPrice() * quantity;
+    }
 }
