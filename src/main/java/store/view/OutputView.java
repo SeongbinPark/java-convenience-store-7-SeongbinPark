9a60d0b8ea2555c571d4aa1bace68ff69356018d
SeongbinPark <hyucse22@gmail.com>
2024-11-11 15:43:12 +0900

    refactor(OutputView): 명세에 맞게 메서드 추출
    한 메서드는 한 일만 함

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -17,50 +17,53 @@ public class OutputView {
 
     public void printProducts(final List<Product> products) {
         for (final Product product : products) {
-            String promotionType = "";
-            if (product.hasPromotion()) {
-                promotionType = " " + product.getPromotionType();
-            }
+            String promotionType = getPromotionTypeString(product);
 
-            if (product.getTotalStock() > 0) {
-                System.out.printf(OutputMessages.PRODUCT_AVAILABLE.getMessage(),
-                        product.getName(),
-                        product.getPrice(),
-                        product.getTotalStock(),
-                        promotionType);
-            }
+            printAvailableProduct(product, promotionType);
 
-            if (product.getTotalStock() <= 0) {
-                System.out.printf(OutputMessages.PRODUCT_UNAVAILABLE.getMessage(),
-                        product.getName(),
-                        product.getPrice(),
-                        promotionType);
-            }
+            printUnavailableProduct(product, promotionType);
         }
         System.out.println();
     }
 
+    private static void printUnavailableProduct(Product product, String promotionType) {
+        if (product.getTotalStock() <= 0) {
+            System.out.printf(OutputMessages.PRODUCT_UNAVAILABLE.getMessage(),
+                    product.getName(),
+                    product.getPrice(),
+                    promotionType);
+        }
+    }
+
+    private static void printAvailableProduct(Product product, String promotionType) {
+        if (product.getTotalStock() > 0) {
+            System.out.printf(OutputMessages.PRODUCT_AVAILABLE.getMessage(),
+                    product.getName(),
+                    product.getPrice(),
+                    product.getTotalStock(),
+                    promotionType);
+        }
+    }
+
+    private static String getPromotionTypeString(Product product) {
+        String promotionType = "";
+        if (product.hasPromotion()) {
+            promotionType = " " + product.getPromotionType();
+        }
+        return promotionType;
+    }
+
     public void printReceipt(final Receipt receipt) {
         final StringBuilder sb = new StringBuilder();
-        sb.append(OutputMessages.RECEIPT_HEADER.getMessage()).append(LINE_SEPARATOR);
-        sb.append(String.format(OutputMessages.RECEIPT_PRODUCT_HEADER.getMessage(),
-                "상품명", "수량", "금액")).append(LINE_SEPARATOR); // 제목의 너비 조정
+        appendProductHeader(sb);
+        appendOrderItems(receipt, sb);
+        appendFreeItems(receipt, sb);
+        appendTotalSectionHeader(sb);
+        appendTotal(receipt, sb);
+        System.out.print(sb);
+    }
 
-        for (final OrderItem item : receipt.getOrderItems()) {
-            sb.append(String.format("%-12s %4d %,10d%n",
-                    item.product().getName(),
-                    item.quantity(),
-                    item.calculateTotalPrice()));
-        }
-
-        sb.append(OutputMessages.RECEIPT_SECTION_FREE_ITEMS.getMessage()).append(LINE_SEPARATOR);
-        for (final FreeItem item : receipt.getFreeItems()) {
-            sb.append(String.format("%-12s %4d%n",
-                    item.product().getName(),
-                    item.quantity()));
-        }
-
-        sb.append(OutputMessages.RECEIPT_SECTION_TOTAL.getMessage()).append(LINE_SEPARATOR);
+    private static void appendTotal(Receipt receipt, StringBuilder sb) {
         sb.append(String.format("%-12s %4d %,10d%n",
                 OutputMessages.TOTAL_PURCHASE.getMessage(), receipt.getTotalQuantity(), receipt.getTotalAmount()));
         sb.append(String.format("%-12s %,14d%n",
@@ -69,8 +72,34 @@ public class OutputView {
                 OutputMessages.MEMBERSHIP_DISCOUNT.getMessage(), -receipt.getMembershipDiscount()));
         sb.append(String.format("%-12s %,14d%n",
                 OutputMessages.FINAL_AMOUNT.getMessage(), receipt.getFinalAmount()));
+    }
 
-        System.out.print(sb);
+    private static void appendTotalSectionHeader(StringBuilder sb) {
+        sb.append(OutputMessages.RECEIPT_SECTION_TOTAL.getMessage()).append(LINE_SEPARATOR);
+    }
+
+    private static void appendFreeItems(Receipt receipt, StringBuilder sb) {
+        sb.append(OutputMessages.RECEIPT_SECTION_FREE_ITEMS.getMessage()).append(LINE_SEPARATOR);
+        for (final FreeItem item : receipt.getFreeItems()) {
+            sb.append(String.format("%-12s %4d%n",
+                    item.product().getName(),
+                    item.quantity()));
+        }
+    }
+
+    private static void appendOrderItems(Receipt receipt, StringBuilder sb) {
+        for (final OrderItem item : receipt.getOrderItems()) {
+            sb.append(String.format("%-12s %4d %,10d%n",
+                    item.product().getName(),
+                    item.quantity(),
+                    item.calculateTotalPrice()));
+        }
+    }
+
+    private static void appendProductHeader(StringBuilder sb) {
+        sb.append(OutputMessages.RECEIPT_HEADER.getMessage()).append(LINE_SEPARATOR);
+        sb.append(String.format(OutputMessages.RECEIPT_PRODUCT_HEADER.getMessage(),
+                "상품명", "수량", "금액")).append(LINE_SEPARATOR); // 제목의 너비 조정
     }
 
     public void printError(final String message) {
