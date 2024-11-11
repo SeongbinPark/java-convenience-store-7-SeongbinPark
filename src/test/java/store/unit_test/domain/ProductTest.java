18e9abb02e4ec29dae2c0a16b2aa38544d7c8318
SeongbinPark <hyucse22@gmail.com>
2024-11-11 19:13:06 +0900

    test(ProductTest): Product 클래스 단위테스트 추가
    validateInsufficientStock

--- src/test/java/store/unit_test/domain/ProductTest.java
+++ src/test/java/store/unit_test/domain/ProductTest.java
@@ -1,6 +1,7 @@
 package store.unit_test.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,5 +28,17 @@ class ProductTest {
         assertThat(product.getPromotionStock()).isEqualTo(5);
         assertThat(product.getNormalStock()).isEqualTo(7);
         assertThat(product.getTotalStock()).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("재고가 부족한 경우 예외가 발생한다")
+    void validateInsufficientStock() {
+        // given
+        Product product = new Product("콜라", 1000, 5, 5, "탄산2+1");
+
+        // when & then
+        assertThatThrownBy(() -> product.processOrder(11, 5))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
     }
 }
